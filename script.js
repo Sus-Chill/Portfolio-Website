af2ea0ea84c2582dc624f0b55516321f51593850
Susil Thapa <152716085+suschill@users.noreply.github.com>
2024-08-18 11:22:26 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Responsive Navbar Toggle
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
